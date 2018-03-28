f0ff63cf3b1a63e59833d1819492b6b7d583e1dd
ysk H <yskhara97@gmail.com>
2018-03-28 14:39:21 +0900

    idk

--- src/base_teleop_joy.cpp
+++ src/base_teleop_joy.cpp
@@ -9,10 +9,6 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
-static constexpr int LeftThumbY = 1;
-static constexpr int LeftThumbX = 0;
-static constexpr int RightThumbX = 2;
-
 class BaseTeleop
 {
 public:
@@ -23,16 +19,21 @@ private:
 
   ros::NodeHandle nh_;
 
-  int linear_, angular_;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
 
+	static int AxisLeftThumbX;
+	static int AxisLeftThumbY;
+	static int AxisRightThumbX;
+	static int ButtonRB;
 };
 
+int BaseTeleop::AxisLeftThumbX = 0;
+int BaseTeleop::AxisLeftThumbY = 1;
+int BaseTeleop::AxisRightThumbX = 2;
+int BaseTeleop::ButtonRB = 4;
 
-BaseTeleop::BaseTeleop():
-  linear_(1),
-  angular_(2)
+BaseTeleop::BaseTeleop()
 {
 
 
@@ -41,17 +42,31 @@ BaseTeleop::BaseTeleop():
 
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &BaseTeleop::joyCallback, this);
 
+	nh_.getParam("AxisLeftThumbX", AxisLeftThumbX);
+	nh_.getParam("AxisLeftThumbY", AxisLeftThumbY);
+	nh_.getParam("AxisRightThumbX", AxisRightThumbX);
+	nh_.getParam("ButtonRB", ButtonRB);
+
 }
 
 void BaseTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  geometry_msgs::Twist twist;
+	geometry_msgs::Twist twist;
 
-  twist.linear.x = 5.0 * joy->axes[LeftThumbY];
-  twist.linear.y = 5.0 * joy->axes[LeftThumbX];
-  twist.angular.z = 3.0 * joy->axes[RightThumbX];
+	if (joy->buttons[ButtonRB] != 0)
+	{
+		twist.linear.x = 0.5 * joy->axes[AxisLeftThumbY];
+		twist.linear.y = -0.5 * joy->axes[AxisLeftThumbX];
+		twist.angular.z = 0.25 * joy->axes[AxisRightThumbX];
+	}
+	else
+	{
+		twist.linear.x = 5.0 * joy->axes[AxisLeftThumbY];
+		twist.linear.y = -5.0 * joy->axes[AxisLeftThumbX];
+		twist.angular.z = 3.0 * joy->axes[AxisRightThumbX];
+	}
 
-  vel_pub_.publish(twist);
+	vel_pub_.publish(twist);
 }
 
 
