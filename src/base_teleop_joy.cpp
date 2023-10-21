430af8ec506644c14ce2621d56c5c1b13b159544
YskHara <yskhara97@gmail.com>
2023-10-21 23:59:12 +0900

    add nonlinear joystick sensitivity

--- src/base_teleop_joy.cpp
+++ src/base_teleop_joy.cpp
@@ -8,6 +8,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
+#include <cmath>
 
 static constexpr int LeftThumbY = 1;
 static constexpr int LeftThumbX = 0;
@@ -30,6 +31,9 @@ private:
     double max_lin_turbo;
     double max_ang;
     double max_ang_turbo;
+
+    double sensitivity_adj_pow_xy;
+    double sensitivity_adj_pow_z;
 
     static int AxisLeftThumbX;
     static int AxisLeftThumbY;
@@ -60,6 +64,8 @@ BaseTeleop::BaseTeleop()
     _nh.param("max_lin_turbo", this->max_lin_turbo, this->max_lin);
     _nh.param("max_ang", this->max_ang, M_PI);
     _nh.param("max_ang_turbo", this->max_ang_turbo, this->max_ang);
+    _nh.param("sensitivity_adj_pow_xy", this->sensitivity_adj_pow_xy, 1.0);
+    _nh.param("sensitivity_adj_pow_z", this->sensitivity_adj_pow_z, 1.0);
 
     ROS_INFO("max_lin: %lf", this->max_lin);
     ROS_INFO("max_lin_turbo: %lf", this->max_lin_turbo);
@@ -74,6 +80,14 @@ void BaseTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     double vel_x = joy->axes[AxisLeftThumbY];
     double vel_y = joy->axes[AxisLeftThumbX];
     double vel_z = joy->axes[AxisRightThumbX];
+
+    int vel_x_sgn = vel_x < 0 ? -1 : 1;
+    int vel_y_sgn = vel_y < 0 ? -1 : 1;
+    int vel_z_sgn = vel_z < 0 ? -1 : 1;
+    
+    vel_x = vel_x_sgn * std::abs(std::pow(vel_x, this->sensitivity_adj_pow_xy));
+    vel_y = vel_y_sgn * std::abs(std::pow(vel_y, this->sensitivity_adj_pow_xy));
+    vel_z = vel_z_sgn * std::abs(std::pow(vel_z, this->sensitivity_adj_pow_z));
 
     double vel_norm = hypot(vel_x, vel_y);
     if (vel_norm > 1.0)
